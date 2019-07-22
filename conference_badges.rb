0266637480f9f06639abea5a03992c8ad744c920
Natalie Taktachev <ntaktach@gmail.com>
2019-07-22 02:22:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |attendee|
+   "Hello, my name is #{attendee}."
+  end
+end
+
+
+def assign_rooms(attendees)
+    attendees.each_with_index.map do |attendee, room|
+      "Hello, #{attendee}! You'll be assigned to room #{room+1}!"
+  end
+end
+
+def printer(attendees)
+end
